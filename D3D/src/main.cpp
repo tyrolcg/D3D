a3550524d25e936e854df1c4d6b795846ad01aae
Tyrol Ema <tyrolcg.ema@gmail.com>
2025-01-20 18:09:28 +0900

    add デバッグ用処理を追加

--- D3D/src/main.cpp
+++ D3D/src/main.cpp
@@ -9,6 +9,15 @@
 /// <returns></returns>
 int wmain(int argc, wchar_t argv, wchar_t** evnp)
 {
+	// プログラム終了時のメモリリークチェック、出力を行う
+#if defined(DEBUG) || defined(_DEBUG)
+	{
+		_CrtSetDbgFlag(
+			_CRTDBG_ALLOC_MEM_DF    // デバッグ用のヒープメモリが使われるようになる
+			| _CRTDBG_LEAK_CHECK_DF // プログラム終了時にメモリリークチェックを行う
+		);
+	}
+#endif
 	App app(960, 540);
 	app.Run();
 
